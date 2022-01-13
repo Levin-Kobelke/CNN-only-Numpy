d7efa8b44677852e12c36275ac32a315f22eb3f6
Levin <96979510+Levin-Kobelke@users.noreply.github.com>
2022-01-13 17:26:10 +0100

    reupload

--- numpyNet/imports/loss.py
+++
@@ -1,9 +0,0 @@
-#defining a common loss function that just calls the part loss function
-#and calculates the mean over all samples
-class Loss:
-    
-    def calculate(self, output, y):
-        
-        sample_losses = self.forward(output, y)
-        data_loss     = np.mean(sample_losses)
-        return(data_loss)
